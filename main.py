ac9e44c4d440d882bf08af82d3bf2b1c4e1db389
Rauko <Nils.Bonefass@outlook.de>
2025-03-19 18:46:06 +0100

    Asteroids Spawn, Player Moves

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 import pygame
 from constants import *
-
+from player import *
+from asteroid import *
+from asteroidfield import *
 def main():
     pygame.init()
+
     clock = pygame.time.Clock()
     dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    asteroidfield = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         screen.fill((0,0,0))
+
+        for obj in drawable:
+            obj.draw(screen)
+    
+        
+        updatable.update(dt)
+
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
