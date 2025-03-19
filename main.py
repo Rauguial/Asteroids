0fa6280922fa81e3a0e2bb6a58a0e0f9727df5f1
Rauko <Nils.Bonefass@outlook.de>
2025-03-19 12:00:48 +0100

    FPS

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,5 +15,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        ms = clock.tick(60)
+        dt = ms / 1000
+
+
 if __name__ == "__main__":
     main()
